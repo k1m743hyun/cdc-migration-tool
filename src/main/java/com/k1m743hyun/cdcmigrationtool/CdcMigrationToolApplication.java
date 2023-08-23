ea8154282bed1522144265d0ec5b259808833cee
Taehyun Kim <k1m743hyun@icloud.com>
2023-08-23 17:00:57 +0900

    Create cdc-migration-tool

---
+++ src/main/java/com/k1m743hyun/cdcmigrationtool/CdcMigrationToolApplication.java
@@ -0,0 +1,13 @@
+package com.k1m743hyun.cdcmigrationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcMigrationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcMigrationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k1m743hyun/cdcmigrationtool/CdcMigrationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k1m743hyun.cdcmigrationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcMigrationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
